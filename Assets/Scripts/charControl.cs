4d67bba366181248092b6dd4c214b75ec9e9dc64
yogo <jared88045@gmail.com>
2021-12-05 16:59:03 +0800

    creat

---
+++ Assets/Scripts/charControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charControl : MonoBehaviour {
+
+  Animator myAnimator;
+  Rigidbody2D  myRigid; 
+  bool facingRight;
+  public float speedScale;
+
+  // Start is called before the first frame update
+  void Start()     {
+    myAnimator = GetComponent<Animator>();
+    myRigid = GetComponent<Rigidbody2D>();
+    facingRight = true;
+  }
+
+  // Update is called once per frame
+  void Update() {
+      float move = Input.GetAxis("Horizontal");
+//      Debug.Log("按鍵數值= " + move);
+      myAnimator.SetFloat("Speed", Mathf.Abs(move));
+      myRigid.velocity = new Vector2 (move * speedScale, myRigid.velocity.y);
+      if (move > 0 && facingRight != true) {
+        //條件成立時的反應：圖片轉向
+        Flip();
+      }
+      else if (move < 0 && facingRight == true) {
+        //圖片轉向
+        Flip();
+      }
+  }
+
+  void Flip() {
+    Vector3 temp = transform.localScale;
+    temp.x = temp.x * -1.0f;
+    transform.localScale = temp;
+    facingRight = !facingRight;
+  }
+}
---
+++ Assets/Scripts/trigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+      
+    }
+    void  OnTriggerEnter2D (Collider2D other) {
+        
+        if (other.name == "spheria") {
+          //Destroy (this.gameObject);
+          this.gameObject.SetActive (false);
+        }
+    }
+    void  OnTriggerStay2D (Collider2D other) {
+        Debug.Log ("stay"); 
+    }
+    void  OnTriggerExit2D (Collider2D other) {
+      if (other.name == "spheria") {
+        Debug.Log (this.gameObject.activeSelf);;
+      }
+
+    }
+}
